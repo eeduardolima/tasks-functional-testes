98281af9d1a3d885db50cd6e93d8190d562e94ea
Eduardo Lima <eduardooliveiradelima19@gmail.com>
2022-10-16 19:58:29 -0300

    add paralelismo de execucao

--- src/test/java/br/com/wcaquino/functional/TasksTest.java
+++ src/test/java/br/com/wcaquino/functional/TasksTest.java
@@ -7,64 +7,65 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class TasksTest {
 
-    WebDriver driver = new ChromeDriver();
-
-    public void esperar(WebElement webElement){
-        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(webElement));
-    }
-
-    @After
-    public void rodarApos(){
-        driver.quit();
-    }
-
-    @Test
-    public void testAmbiente(){
-        driver.navigate().to("http://www.google.com");
-    }
-
-    @Test
-    public void deveSalvarTarefaComSucesso() throws InterruptedException {
+    public WebDriver acessarAplicacao() throws MalformedURLException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.17:4444"), chromeOptions);
         driver.navigate().to("http://localhost:8001/tasks/");
+        return driver;
+    }
+
+    @Test
+    public void deveSalvarTarefaComSucesso() throws InterruptedException, MalformedURLException {
+        WebDriver driver = acessarAplicacao();
         driver.findElement(By.id("addTodo")).click();
         driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
         driver.findElement(By.id("dueDate")).sendKeys("20/10/2022");
         driver.findElement(By.id("saveButton")).click();
         Assert.assertEquals(driver.findElement(By.id("message")).getText(),"Success!");
+        driver.quit();
     }
 
     @Test
-    public void naoDeveSalvarTarefaSemDescricao() throws InterruptedException {
-        driver.navigate().to("http://localhost:8001/tasks/");
+    public void naoDeveSalvarTarefaSemDescricao() throws InterruptedException, MalformedURLException {
+        WebDriver driver = acessarAplicacao();
         driver.findElement(By.id("addTodo")).click();
         driver.findElement(By.id("dueDate")).sendKeys("20/10/2022");
         driver.findElement(By.id("saveButton")).click();
         Assert.assertEquals(driver.findElement(By.id("message")).getText(),"Fill the task description");
+        driver.quit();
     }
 
     @Test
-    public void naoDeveSalvarTarefaSemData() throws InterruptedException {
-        driver.navigate().to("http://localhost:8001/tasks/");
+    public void naoDeveSalvarTarefaSemData() throws InterruptedException, MalformedURLException {
+        WebDriver driver = acessarAplicacao();
         driver.findElement(By.id("addTodo")).click();
         driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
         driver.findElement(By.id("saveButton")).click();
         Assert.assertEquals(driver.findElement(By.id("message")).getText(),"Fill the due date");
+        driver.quit();
     }
 
     @Test
-    public void deveSalvarTarefaComDataPassada() throws InterruptedException {
+    public void deveSalvarTarefaComDataPassada() throws InterruptedException, MalformedURLException {
+        WebDriver driver = acessarAplicacao();
         driver.navigate().to("http://localhost:8001/tasks/");
         driver.findElement(By.id("addTodo")).click();
         driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
         driver.findElement(By.id("dueDate")).sendKeys("20/10/2012");
         driver.findElement(By.id("saveButton")).click();
         Assert.assertEquals(driver.findElement(By.id("message")).getText(),"Due date must not be in past");
+        driver.quit();
     }
 }
